6d7645c47cc5aeff6917740c35da463a74225e77
stuarttiedemann <stuarttiedemann@yahoo.com>
2015-12-01 13:51:18 -0500

    Added ability to pick a departure location or allow to default to center of country

--- controller.js
+++ controller.js
@@ -12,13 +12,11 @@ angular.module('myApp',[]).controller('myController',function($scope){
 
 	$scope.planeSelector =function(){
 	    var numberOfPilots = $('.pilot:checked').val();
-		console.log(numberOfPilots);
 		if(numberOfPilots == 1){
 			$scope.planes = mustangs1;
 		}else{
 			$scope.planes = mustangs2;
 		}
-		console.log($scope.mustangs1);
 	}
 	
 	// Create a new google map in the html div with id = "map"
@@ -28,11 +26,10 @@ angular.module('myApp',[]).controller('myController',function($scope){
 	var lng;
 	$scope.geocodeAddress = function() {
 		address = $scope.address;
-		console.log(address);
 		var url = "https://maps.googleapis.com/maps/api/geocode/json?address="+address+"&key="+apiKey;
 
 	    $.getJSON(url, function(data){
-	    	console.log(data)
+	    	// console.log(data)
 	    	lat = data.results[0].geometry.location.lat;
 	    	lng = data.results[0].geometry.location.lng;
 	    	var map;
@@ -47,43 +44,52 @@ angular.module('myApp',[]).controller('myController',function($scope){
 			position: new google.maps.LatLng(lat, lng),
 	    	})
 	    	// $('#address').val('');
-
+	    })
+	}
 
 			$scope.range = function($index){
-				console.log("hi");
 				var map;
 				var infowindow;
-				var pyrmont = {lat: lat, lng: lng};
-				  map = new google.maps.Map(document.getElementById('map'), {
+				  // var address = $('#address').val;
+				  console.log("The value of address is: "+address);
+				if(address ==''){
+					var mapOption = {
+					zoom:4,
+					center: new google.maps.LatLng(40.000,-98.000),
+					mapTypeId: google.maps.mapTypeId
+					}
+					var pyrmont = {lat:40.000, lng: -98.000};
+					map = new google.maps.Map(document.getElementById('map'), {
 				  	center: pyrmont,
 				  	zoom: 4
-				  });
-				  // var address = $('#address').val;
-				  console.log(address);
-				if(address ==''){
+				  	});
 					var marker = new google.maps.Marker({
 					map: $scope.map,
-					position: new google.maps.LatLng(40.000,-98.000)
+					position: new google.maps.LatLng(40.000,-98.000),
 					});
+					var circle = new google.maps.Circle({
+				  		map: map,
+				  		radius: 1503824,    // 812 nm (1852 meters per nautical mile)
+				  		fillColor: '#AA0000'
+					});
+					console.log(circle);
+				  	circle.bindTo('center', marker, 'position');
 				}else{
+					var pyrmont = {lat: lat, lng: lng};
+				  	map = new google.maps.Map(document.getElementById('map'), {
+				  	center: pyrmont,
+				  	zoom: 4
+				  	});
 					var marker = new google.maps.Marker({
 						map: $scope.map,
 						position: new google.maps.LatLng(lat, lng)
 				    })
+				    var circle = new google.maps.Circle({
+				  		map: map,
+				  		radius: 1503824,    // 812 nm (1852 meters per nautical mile)
+				  		fillColor: '#AA0000'
+					});
+				  	circle.bindTo('center', marker, 'position');
 				}
-			    var circle = new google.maps.Circle({
-				  map: map,
-				  radius: 1503824,    // 812 nm (1852 meters per nautical mile)
-				  fillColor: '#AA0000'
-				});
-				var circle2 = new google.maps.Circle({
-				  map: map,
-				  radius: 10093,    // 10 miles in metres
-				  fillColor: '#00AA00'
-				});
-				circle.bindTo('center', marker, 'position');
-				circle2.bindTo('center', marker, 'position');
 			}
-	    })
-	}
-})
+	})
